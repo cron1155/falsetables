002797d7ca57fa4329307c379c2a0d0dc4c9592b
chiuzon <40052481+cron1155@users.noreply.github.com>
2023-02-22 12:41:09 +0200

    Parse CLI args

--- src/cli.ts
+++ src/cli.ts
@@ -1,8 +1,46 @@
 
+function parseArgs(argsDef: { argName: string, argType: string }[], args: string[]) {
 
+    const argsData = new Map()
 
-async function main() {
+    if (args[0]) {
+        argsData.set("input", args[0])
+    }
 
+    for (const argDef of argsDef) {
+        const argIndex = args.findIndex((val) => {
+            if (val.substring(0, 2) === "--") {
+                return val.substring(2, val.length) === argDef.argName
+            } else if (val[0] === "-") {
+                return val.substring(1, val.length) === argDef.argName
+            }
+        })
+
+        if (argIndex === -1) continue;
+
+        const argValue = args[argIndex + 1]
+
+        if (!argValue) {
+            throw new Error(`${argDef.argName} value is undefined!`);
+        }
+
+        argsData.set(argDef.argName, argValue)
+    }
+
+    return argsData
 }
 
-main();
+async function main() {
+    const args = parseArgs([{
+        argName: "output-type", 'argType': "string"
+    }], process.argv.slice(2))
+
+    if (!args.get("input")) {
+        console.info("A")
+    }
+
+    console.log(args.get("input"))
+    console.log(args.get("output-type"))
+}
+
+main()
